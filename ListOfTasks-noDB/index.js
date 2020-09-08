8720503e21eda1519e92fd369c5f20a53c0eaeaa
Manju <manju.s2017cseb@sece.ac.in>
2020-09-08 15:37:11 +0530

    CRUD_TASK & PROJECT MANAGEMENT

--- ListOfTasks-noDB/index.js
+++ ListOfTasks-noDB/index.js
@@ -8,21 +8,15 @@ const itemsRouter = require('./task');
 // create new app
 const app = express();
 app.use(express.json());
-// use it before all route definitions
-// allowing below URL to access these APIs end-points
-// you can replace this URL(http://localhost:8100) with your
-// application URL from where you are calling these APIs
+
 app.use(cors({origin: 'http://localhost:8100'}));
 
-/* this '/items' URL will have two end-points:
-→ localhost:3000/items/ (this returns array of objects)
-→ localhost:3000/items/:id (this returns single object)
-*/
+
 app.use('/items', itemsRouter);
 
 // default URL to API
 app.use('/', function(req, res) {
-    res.send('node-ex-api works :-)');
+    res.send('Api works');
 });
 
 const server = http.createServer(app);
